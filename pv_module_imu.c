9e368344a35b43bcda317c20168844444b758826
Iuro Nascimento <iurobpn@gmail.com>
2015-09-14 17:37:03 -0300

    Código adequado a alterações de da commmit 92137f5

--- pv_module_imu.c
+++ pv_module_imu.c
@@ -33,8 +33,6 @@
 portTickType lastWakeTime;
 char str[256];
 GPIOPin LED4;
-int32_t bmp = 18001;
-struct L3GD20 gyro;
 static USART_TypeDef *USARTn = USART6;
 static I2C_TypeDef *I2Cn;
 
@@ -233,33 +231,32 @@ void module_imu_init()
 	c_io_imuAdafruit_init(I2Cn);
 	c_common_usart6_init(BAUDRATE);
 
-	c_io_l3gd20_init(&gyro,20);
 	c_common_usart_puts(USARTn,"Adafruit 10DOF Tester");
 	c_common_usart_putchar(USARTn,'\n');
 
 	/* Initialise the sensors */
-	if(!c_io_lsm303_initAccel(I2Cn)) {
-		/* There was a problem detecting the ADXL345 ... check your connections */
-		c_common_usart_puts(USARTn,"Ooops, no LSM303 detected ... Check your wiring!");
-		c_common_usart_putchar(USARTn,'\n');
-		while(1);
-	}
-	if(!c_io_lsm303_initMag(I2Cn)) {
-		/* There was a problem detecting the LSM303 ... check your connections */
-		c_common_usart_puts(USARTn,"Ooops, no LSM303 detected ... Check your wiring!");
-		c_common_usart_putchar(USARTn,'\n');
-		while(1);
-	}
-	if(!c_io_bmp180_init(I2Cn,bmp)) {
-		/* There was a problem detecting the BMP085 ... check your connections */
-		c_common_usart_puts(USARTn,"Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
-		while(1);
-	}
-	if(!c_io_l3gd20_begin(&gyro,GYRO_RANGE_250DPS)) {
-		/* There was a problem detecting the L3GD20 ... check your connections */
-		c_common_usart_puts(USARTn,"Ooops, no L3GD20 detected ... Check your wiring or I2C ADDR!");
-		while(1);
-	}
+		if(!c_io_lsm303_initAccel(I2Cn)) {
+			/* There was a problem detecting the ADXL345 ... check your connections */
+			c_common_usart_puts(USARTn,"Ooops, no LSM303 detected ... Check your wiring!");
+			c_common_usart_putchar(USARTn,'\n');
+			while(1);
+		}
+		if(!c_io_lsm303_initMag(I2Cn)) {
+			/* There was a problem detecting the LSM303 ... check your connections */
+			c_common_usart_puts(USARTn,"Ooops, no LSM303 detected ... Check your wiring!");
+			c_common_usart_putchar(USARTn,'\n');
+			while(1);
+		}
+		if(!c_io_bmp180_init(I2Cn,BMP180_MODE_ULTRAHIGHRES)) {
+			/* There was a problem detecting the BMP085 ... check your connections */
+			c_common_usart_puts(USARTn,"Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+			while(1);
+		}
+		if(!c_io_l3gd20_init(GYRO_RANGE_250DPS)) {
+			/* There was a problem detecting the L3GD20 ... check your connections */
+			c_common_usart_puts(USARTn,"Ooops, no L3GD20 detected ... Check your wiring or I2C ADDR!");
+			while(1);
+		}
 
 	/* Display some basic information on this sensor */
 	//displaySensorDetails();
@@ -289,90 +286,7 @@ void module_imu_run()
 		/*----------------------Tratamento da IMU---------------------*/
 		/* Pega e trata os valores da imu */
 		//c_io_imu_getRaw(oInputData.imuOutput.accRaw, oInputData.imuOutput.gyrRaw, oInputData.imuOutput.magRaw,sample_time_gyro_us);
-		/* Get a new sensor event */
-		sensors_event_t event;
 
-		/* Display the results (acceleration is measured in m/s^2) */
-		c_io_lsm303_getDataAccel();
-		c_common_usart_puts(USARTn,"ACCEL ");
-		c_common_usart_puts(USARTn,"X: ");
-		c_common_utils_floatToString(event.acceleration.x,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"Y: ");
-		c_common_utils_floatToString(event.acceleration.y,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"Z: ");
-		c_common_utils_floatToString(event.acceleration.z,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"m/s^2 ");
-		c_common_usart_putchar(USARTn,'\n');
-
-		/* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-		c_io_lsm303_getMagData();
-		c_common_usart_puts(USARTn,"MAG   ");
-		c_common_usart_puts(USARTn,"X: ");
-		c_common_utils_floatToString(event.magnetic.x,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"Y: ");
-		c_common_utils_floatToString(event.magnetic.y,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"Z: ");
-		c_common_utils_floatToString(event.magnetic.z,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"uT");
-		c_common_usart_putchar(USARTn,'\n');
-
-		/* Display the results (gyrocope values in rad/s) */
-		c_io_l3gd20_getEvent(&gyro, &event);
-		c_common_usart_puts(USARTn,"GYRO  ");
-		c_common_usart_puts(USARTn,"X: ");
-		c_common_utils_floatToString(event.gyro.x,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"Y: ");
-		c_common_utils_floatToString(event.gyro.y,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"Z: ");
-		c_common_utils_floatToString(event.gyro.z,tmp,2);
-		c_common_usart_puts(USARTn,tmp);
-		c_common_usart_puts(USARTn,"  ");
-		c_common_usart_puts(USARTn,"rad/s ");
-		c_common_usart_putchar(USARTn,'\n');
-
-		/* Display the pressure sensor results (barometric pressure is measure in hPa) */
-		c_io_bmp180_getData(bmp,&event);
-		if (event.pressure)
-		{
-			/* Display atmospheric pressure in hPa */
-			c_common_usart_puts(USARTn,"PRESS ");
-			c_common_utils_floatToString(event.pressure,tmp,2);
-			c_common_usart_puts(USARTn,tmp);
-			c_common_usart_puts(USARTn," hPa, ");
-			/* Display ambient temperature in C */
-			float temperature;
-			c_io_bmp180_getTemperature(&temperature);
-			c_common_utils_floatToString(event.temperature,tmp,2);
-			c_common_usart_puts(USARTn,tmp);
-			c_common_usart_puts(USARTn," C, ");
-			/* Then convert the atmospheric pressure, SLP and temp to altitude    */
-			/* Update this next line with the current SLP for better results      */
-			float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-
-			float fl_tmp = c_io_bmp180_pressureToAltitude(seaLevelPressure,event.pressure);
-			c_common_utils_floatToString(fl_tmp,tmp,2);
-			c_common_usart_puts(USARTn,tmp);
-			c_common_usart_puts(USARTn," m");
-			c_common_usart_putchar(USARTn,'\n');
-		}
-
-		c_common_usart_putchar(USARTn,'\n');
 
 		/* toggle pin for debug */
 		c_common_gpio_toggle(LED4);
